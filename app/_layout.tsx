f04a24d19b94387330225c60fa0b9a1c7dbb6170
Lam Dao <lam.dao@edu.sait.ca>
2025-09-26 19:39:12 -0600

    Remove login function

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,7 +7,6 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
-import { UserContextProvider } from "@/context/authContext";
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
@@ -18,7 +17,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <UserContextProvider>
+
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -35,6 +34,6 @@ export default function RootLayout() {
         </Stack>
         <StatusBar style="auto" />
       </ThemeProvider>
-    </UserContextProvider>
+
   ); 
 }
