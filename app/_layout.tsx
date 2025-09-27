d4cd0b3c5d5eb1111b4c73f1f73f948d76f8936b
joywong1228 <joywong1228@gmail.com>
2025-09-27 11:59:46 -0600

    steamline the entire app's style

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,10 +4,9 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
+import { SafeAreaView } from "react-native-safe-area-context"; // safe area wrapper
 
-import { UserContextProvider } from "@/context/authContext";
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
@@ -18,23 +17,25 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <UserContextProvider>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <SafeAreaView
+        style={{ flex: 1, backgroundColor: "transparent" }}
+        edges={["top"]}
+      >
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
           <Stack.Screen
             name="modal"
             options={{ presentation: "modal", title: "Modal" }}
           />
-          <Stack.Screen
-            name="sign_in"
-            options={{
-              headerShown: false,
-            }}
-          />
+          <Stack.Screen name="sign_in" options={{ headerShown: false }} />
         </Stack>
-        <StatusBar style="auto" />
-      </ThemeProvider>
-    </UserContextProvider>
-  ); 
+        {/* <StatusBar style="auto" /> */}
+      </SafeAreaView>
+    </ThemeProvider>
+  );
 }
