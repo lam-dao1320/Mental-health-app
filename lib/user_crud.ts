74afb5314fbe3f16e6131bb3eaa0ae1b7f6a6772
joywong1228 <joywong1228@gmail.com>
2025-10-28 11:07:44 -0600

    bug fix: authenication error

--- lib/user_crud.ts
+++ lib/user_crud.ts
@@ -2,36 +2,34 @@ import { UserProfile } from "./object_types";
 import { supabase } from "./supabase";
 
 export async function getUserByEmail(email: string) {
-    const { data, error } = await supabase
-        .from('user_profiles')
-        .select('*')
-        .eq('email', email)
-        .single()
-    if (error) {
-        console.error(`Error fetching user with email ${email}: `, error);
-        throw error;
-    }
-    return data;
+  const { data, error } = await supabase
+    .from("user_profiles")
+    .select("*")
+    .eq("email", email)
+    .maybeSingle();
+  if (error) {
+    console.error(`Error fetching user with email ${email}: `, error);
+    throw error;
+  }
+  return data;
 }
 
 export async function addUser(user: UserProfile) {
-    const { data, error } = await supabase
-        .from('user_profiles')
-        .insert(user)
-    if (error) {
-        // console.error("Error registering user: ", error);
-        throw error;
-    }
-    return data;
+  const { data, error } = await supabase.from("user_profiles").insert(user);
+  if (error) {
+    // console.error("Error registering user: ", error);
+    throw error;
+  }
+  return data;
 }
 
 export async function updateUser(user: UserProfile) {
-    const { data, error } = await supabase
-        .from('user_profiles')
-        .update(user)
-        .eq('email', user.email)
-    if (error) {
-        throw error;
-    }
-    return data;
+  const { data, error } = await supabase
+    .from("user_profiles")
+    .update(user)
+    .eq("email", user.email);
+  if (error) {
+    throw error;
+  }
+  return data;
 }
